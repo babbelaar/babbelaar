cd789fe3d3694d13fdf07ca55ca5e6258c87abec
Tristan Gerritsen <code@thewoosh.org>
2024-06-29 23:47:11 +0200

    feat: Start working on Visual Studio extension

---
+++ tools/Babbelaar.VisualStudio/Babbelaar.VisualStudio/BabbelaarLanguageServerProvider.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Editor;
+using Microsoft.VisualStudio.Extensibility.LanguageServer;
+using Microsoft.VisualStudio.Extensibility.Shell;
+using Nerdbank.Streams;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO.Pipelines;
+using System.Reflection;
+
+namespace Babbelaar.VisualStudio;
+
+[VisualStudioContribution]
+[Experimental("VSEXTPREVIEW_LSP")]
+public sealed class BabbelaarLanguageServerProvider : LanguageServerProvider
+{
+    [VisualStudioContribution]
+    public static DocumentTypeConfiguration BabbelaarDocumentType => new("babbelaar")
+    {
+        FileExtensions = [".bab"],
+        BaseDocumentType = LanguageServerBaseDocumentType,
+    };
+
+    public override LanguageServerProviderConfiguration LanguageServerProviderConfiguration => new(
+        "%Babbelaar%",
+        [DocumentFilter.FromDocumentType(BabbelaarDocumentType)]);
+
+
+    public override async Task<IDuplexPipe?> CreateServerConnectionAsync(CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+            try
+            {
+                return DoCreateServerConnection();
+            }
+            catch (Exception ex)
+            {
+                bool shouldRetry = await Extensibility.Shell().ShowPromptAsync(ex.ToString(), PromptOptions.RetryCancel, cancellationToken);
+
+                if (!shouldRetry)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+
+    private static DuplexPipe? DoCreateServerConnection()
+    {
+        ProcessStartInfo info = new()
+        {
+            FileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, @"babbelaar-lsp.exe"),
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        Process process = new()
+        {
+            StartInfo = info
+        };
+
+        if (process.Start())
+        {
+            return new DuplexPipe(
+                PipeReader.Create(process.StandardOutput.BaseStream),
+                PipeWriter.Create(process.StandardInput.BaseStream)
+            );
+        }
+
+        return null;
+    }
+}
---
+++ tools/Babbelaar.VisualStudio/Babbelaar.VisualStudio/ExtensionEntrypoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Editor;
+
+namespace Babbelaar.VisualStudio;
+
+/// <summary>
+/// Extension entrypoint for the VisualStudio.Extensibility extension.
+/// </summary>
+[VisualStudioContribution]
+internal class ExtensionEntrypoint : Extension
+{
+    /// <inheritdoc/>
+    public override ExtensionConfiguration ExtensionConfiguration => new()
+    {
+        Metadata = new(
+            id: "Babbelaar.VisualStudio.c0ef11a6-c3be-42ef-b317-1d91ed9e3043",
+            version: this.ExtensionAssemblyVersion,
+            publisherName: "Babbelaar",
+            displayName: "Babbelaar voor Visual Studio",
+            description: "Ondersteuning voor de Babbelaar-taal in Visual Studio"),
+    };
+
+    /// <inheritdoc />
+    protected override void InitializeServices(IServiceCollection serviceCollection)
+    {
+        base.InitializeServices(serviceCollection);
+
+        // You can configure dependency injection here by adding services to the serviceCollection.
+    }
+}
