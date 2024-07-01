992fa2d4dd3334872eefdc9f3a2544f20f09f640
Tristan Gerritsen <code@thewoosh.org>
2024-07-01 19:57:04 +0200

    feat: Add some integration tests

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,10 +1,10 @@
 // Copyright (C) 2024 Tristan Gerritsen <tristan@thewoosh.org>
 // All Rights Reserved.
 
-use std::path::PathBuf;
+use std::{path::PathBuf, sync::{Arc, Mutex}};
 
 use babbelaar::{Expression, Lexer, ParseDiagnostic, Parser, Ranged, Statement, Token, Value};
-use babbelaar_interpreter::Interpreter;
+use babbelaar_interpreter::{Debugger, Interpreter};
 
 pub fn parse<'a>(input: &'a str) -> Vec<Statement<'a>> {
     let mut result = Vec::new();
@@ -42,4 +42,33 @@ pub fn interpret_statements(input: &str) {
 pub fn interpret_expression(input: &str) -> Value {
     let expression = parse_expression(input);
     Interpreter::new(()).execute_expression(&expression)
+}
+
+pub fn interpret_and_return_stdout(input: &str) -> Vec<String> {
+    let buffer = Arc::new(Mutex::new(Vec::new()));
+
+    {
+        let mut interpreter = Interpreter::new(TestDebugger {
+            buffer: Arc::clone(&buffer),
+        });
+
+        for statement in parse(input) {
+            interpreter.execute(&statement)
+        }
+    }
+
+
+    Arc::try_unwrap(buffer).unwrap().into_inner().unwrap()
+}
+
+struct TestDebugger {
+    buffer: Arc<Mutex<Vec<String>>>,
+}
+
+impl Debugger for TestDebugger {
+    fn enter_function(&mut self, function: babbelaar_interpreter::DebuggerFunction<'_>, args: &[Value]) {
+        if function.name == "schrijf" {
+            self.buffer.lock().unwrap().push(args[0].to_string());
+        }
+    }
 }
---
+++ tests/tests/stdout.rs
@@ -0,0 +1,51 @@
+// Copyright (C) 2024 Tristan Gerritsen <tristan@thewoosh.org>
+// All Rights Reserved.
+
+use rstest::rstest;
+use tests::interpret_and_return_stdout;
+
+#[rstest]
+#[case(
+    r#"
+        schrijf("Hallo!");
+    "#,
+    &[
+        "Hallo!",
+    ],
+)]
+#[case(
+    r#"
+        stel a = 5;
+        schrijf(€"{a}");
+    "#,
+    &[
+        "5",
+    ],
+)]
+#[case(
+    r#"
+        functie hallo(a: g32) {
+            schrijf(€"{a * 5}");
+        }
+
+        volg i in reeks(0, 10) {
+            hallo(i);
+        }
+    "#,
+    &[
+        "0",
+        "5",
+        "10",
+        "15",
+        "20",
+        "25",
+        "30",
+        "35",
+        "40",
+        "45"
+    ],
+)]
+fn interpret_and_return_stdout_tests(#[case] input: &str, #[case] expected: &[&str]) {
+    let expected: Vec<String> = expected.into_iter().map(|x| x.to_string()).collect();
+    assert_eq!(interpret_and_return_stdout(input), expected);
+}
