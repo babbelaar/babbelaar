75537f8cb993d9e0b6b3461e72c8958f9de9c764
Tristan Gerritsen <code@thewoosh.org>
2024-10-12 15:54:34 +0200

    tests: Voeg integratietests toe aan de LSP
    Deze tests zorgt ervoor dat de LSP goed omgaat met nieuwe tekens. De
    parseerder kan namelijk soms breken, maar door een bestand teken voor
    teken te voeren, kunnen we zekerder zijn dat het werkt.

---
+++ babbelaar/src/constants.rs
@@ -0,0 +1,12 @@
+// Copyright (C) 2024 Tristan Gerritsen <tristan@thewoosh.org>
+// All Rights Reserved.
+
+use std::marker::PhantomData;
+
+pub struct Constants {
+    _marker: PhantomData<String>,
+}
+
+impl Constants {
+    pub const LANGUAGE_ID: &'static str = "babbelaar";
+}
